3e29b61a569f79f2a2170cec8d65a62b73f9b0c8
Hoggspjot <vingisaar@gmail.com>
2025-08-05 00:07:54 +0300

    пробная версия записи в файл

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.nio.file.*;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
 
-
-        Path path = Paths.get("C:\\JavaPractice\\StudyWeek3\\src\\main\\resources\\text.txt");
-
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Введите название файла для записи: ");
+            String nameFile = reader.readLine();
+            Path path = getPathFile(nameFile);
             System.out.println("Введите текст: ");
             while (true) {
                 String str = reader.readLine();
@@ -27,7 +24,7 @@ public class Main {
             }
             String text = sb.toString();
 
-            Files.createFile(path);
+
             Files.write(path,text.getBytes(), StandardOpenOption.APPEND);
 
         } catch (IOException e) {
@@ -35,13 +32,32 @@ public class Main {
         }
 
 
-        try {
-            List<String> list = Files.readAllLines(path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            List<String> list = Files.readAllLines(path);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
 
     }
+
+    public static Path getPathFile(String name){
+        Path path = Paths.get("C:\\JavaPractice\\StudyWeek3\\src\\main\\resources\\"+name+".txt");
+        try {
+            Files.createFile(path);
+            System.out.println("Файл для записи "+name+"создан");
+        } catch (IOException e) {
+            if (e.getClass().getSimpleName().equals(name)) {
+                System.out.println("Файл уже существует!");
+            } else {
+                throw new RuntimeException(e);
+            }
+        }
+        return path;
+    }
+
 }
+
+
+
 
 //"C:\\JavaPractice\\StudyWeek3\\src\\main\\resources\\text.txt"
